c5697de7e5ce44e45903826093a2bdaddb1a99ea
maha441997@gmail.com <maha441997@gmail.com>
2020-06-03 12:26:32 +0530

    portfolio web app

---
+++ server.py
@@ -0,0 +1,59 @@
+from flask import Flask, render_template,url_for,request, redirect
+import csv
+app = Flask(__name__)
+print(__name__)
+
+@app.route('/')
+def my_home():
+    return render_template('./index.html')
+
+@app.route('/<string:page_name>')
+def pages(page_name=None):
+    return render_template(page_name)
+
+def write_to_file(data):
+    with open('database.txt',mode='a') as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        file = database.write(f'\n{email},{subject},{message}')
+        
+def write_to_csv(data):
+    with open('database.csv',mode='a', newline='') as database2:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        csv_writer = csv.writer(database2,delimiter=',',quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email,subject,message])
+        
+        
+@app.route('/submit_form', methods=['POST', 'GET'])
+def sumbit():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('thanku.html')
+        except:
+            return 'Did not saved in database'    
+    else:
+        return "something wrong"
+
+'''
+@app.route('/blog')
+def blog():
+    return 'These are my thoughts on blog'
+
+@app.route('/blog/2020/dogs')
+def blog2():
+    return 'These are dogs'
+
+@app.route('/<username>/<int:post_id>')
+def blog3(username=None,post_id=None):
+    return render_template('./index.html',name=username,id=post_id)
+
+
+set FLASK_ENV=development
+set FLASK_APP=server.py
+flask run
+'''
